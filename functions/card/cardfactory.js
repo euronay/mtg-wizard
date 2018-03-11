c50e7f22ca365f02e4726dc43b1c011994578d43
James Naylor <jamesnaylor85@gmail.com>
2018-03-11 22:23:09 +0000

    Fix meld and flip cards

--- functions/card/cardfactory.js
+++ functions/card/cardfactory.js
@@ -7,9 +7,11 @@ module.exports = class CardFactory{
     static createCard(cardData){
         switch(cardData.layout){
             case "normal":
+            case "meld": // meh, meld is clsoe enough to a normal card - TODO, command to find corresponding card
                 console.log(`Returning normal card ${cardData.name}`);
                 return new Card(cardData);
             case "split":
+            case "flip":
                 console.log(`Returning split card ${cardData.name}`);
                 return new SplitCard(cardData);
             case "transform":
