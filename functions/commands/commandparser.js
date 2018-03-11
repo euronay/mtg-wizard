df3ee2b8abf2ba2439083058281f841fddbde55c
James Naylor <jamesnaylor85@gmail.com>
2018-03-11 22:30:24 +0000

    WIP structure command parser

---
+++ functions/commands/commandparser.js
@@ -0,0 +1,23 @@
+module.exports = class CommandParser {
+
+    static parse(command, context){
+        // no current card
+            // card name
+
+            // from <set>
+
+            // bye
+
+        // card selected
+            // prints/reprints
+
+            //flip
+
+        // returns
+            // search for card(s)
+            // search for specific card
+            // text response
+
+    }
+
+}
